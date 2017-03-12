09d3f0505830f9f1cc155921cb9bb45721925ca7
Priyanka S.Tasnia <stpriyanka2011@gmail.com>
2017-03-12 20:20:30 +0100

    Added upload option

---
+++ OrderPrint/OrderPrint/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OrderPrint.Startup))]
+namespace OrderPrint
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
